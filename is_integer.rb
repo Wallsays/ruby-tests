e33572bdd379dea1efecfe595c06c58b4462e6e5
Dis <sr_5@inbox.ru>
2013-03-05 00:39:00 +0700

    Added is_integer task

---
+++ is_integer.rb
@@ -0,0 +1,10 @@
+test = []
+2.times {test << gets.chop}
+regexp =  /^[0-9]+$/
+test.each do |t|
+  if t.match regexp
+    puts "INTEGER"
+  else
+    puts "NOTINTEGER"
+  end
+end
---
+++ xml_nodes.rb
@@ -0,0 +1,21 @@
+# <classreport>
+# <student studentid='7' marks='20'>Simon</student>
+# <student studentid='2' marks='35'>Blake</student>
+# <student studentid='1' marks='78'>John</student>
+# <student studentid='4' marks='67'>Buddy</student>
+# <student studentid='8' marks='59'>Suzanne</student>
+# <student studentid='6' marks='90'>Tim</student>
+# <student studentid='3' marks='11'>Dave</student>
+# <student studentid='5' marks='45'>Jack</student>
+# </classreport>
+# The program’s output for this XML string would be :
+#
+# John
+# Blake
+# Dave
+# Buddy
+# Jack
+# Tim
+# Simon
+# Suzanne
+
